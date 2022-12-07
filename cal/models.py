ff6a6623ac6b19686c3323d5573a45210acf79c6
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2022-12-06 20:19:26 -0800

    cal working

--- cal/models.py
+++ cal/models.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    student = models.TextField(max_length=32)
+    unit = models.TextField(max_length=10)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
--- cal/urls.py
+++ cal/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'cal'
 urlpatterns = [
-    url(r'^index/$', views.index, name='index'),
-    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
-    url(r'^event/new/$', views.event, name='event_new'),
-	url(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
+    path('index/', views.index, name='index'),
+    path('', views.CalendarView.as_view(), name='calendar'),
+    path('event/new/$', views.event, name='event_new'),
+	path('event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
 ]
